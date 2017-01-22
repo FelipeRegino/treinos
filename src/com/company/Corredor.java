d6111a36ad318d5e93cbdb4e05e1126beb82d004
knuxbbs <knuxbbs@gmail.com>
2017-01-22 19:39:34 -0300

    Criação de classes principais

---
+++ src/com/company/Corredor.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Bruno on 22/01/2017.
+ */
+public class Corredor {
+    String nome;
+    float peso;
+    float altura;
+    int nivel;
+}
---
+++ src/com/company/Corrida.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+
+/**
+ * Created by Bruno on 22/01/2017.
+ */
+public class Corrida {
+    Date dataInicio;
+    float distancia;
+    double tempo;
+    int dificuldade;
+}
